a049a4e4d56598db659c81735a23c209100a68e3
Skrynnik Aleksander <Devid99@yandex.ru>
2022-09-30 21:44:07 +0500

    добавили Example005_if

---
+++ Example005_if/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "александр")
+{
+    Console.WriteLine("Ура, это Александр!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
