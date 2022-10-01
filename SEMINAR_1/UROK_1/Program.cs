5ece8006a06d7259e5abb11f740a97d27843144e
Skrynnik Aleksander <Devid99@yandex.ru>
2022-10-01 16:33:55 +0500

    добавили Example007_Magic

---
+++ SEMINAR_1/UROK_1/Program.cs
@@ -0,0 +1,9 @@
+﻿Cnsole.Clear();
+int x = 6;
+string s = "это строка";
+
+Console.Write("Ведите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(x);
+Console.WriteLine("привет");
+
---
+++ SEMINAR_1/z3/Program.cs
@@ -0,0 +1,28 @@
+﻿/*7. Напишите программу, которая принимает на вход трёхзначное число и 
+на выходе показывает последнюю цифру этого числа.
+	456 -> 6
+	782 -> 2
+	918 -> 8
+*/
+//Console.Clear();
+//int x ;
+//Console.Write("Ведите число ");
+
+// Console.ReadLine();
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine();
+// if (x/100)
+// {
+// Console.Write(x);
+// }
+
+//из семинара
+
+Console.Clear();
+
+Console.Write("Ведите число ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+int num2 = num % 10;
+Console.WriteLine(num2);
+
