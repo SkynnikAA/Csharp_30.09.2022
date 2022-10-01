498cb2c9b5dac5d23784356d58be20fd5469fe90
Skrynnik Aleksander <Devid99@yandex.ru>
2022-10-01 16:39:15 +0500

    перенес папку с семинара

--- SEMINAR_1/UROK_1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Cnsole.Clear();
-int x = 6;
-string s = "это строка";
-
-Console.Write("Ведите число ");
-int num = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(x);
-Console.WriteLine("привет");
-
